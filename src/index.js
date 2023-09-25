4eb7500aa65eb64173967422a7c402f42b67b281
Marco <cwhong8087@gmail.com>
2023-09-25 19:38:41 +0900

    updated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-const hello = 'hello';
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use((ctx) => {
+    ctx.body = 'helloo';
+});
+
+app.listen(4000, () => {
+    console.log('Listening to port 4000');
+});
