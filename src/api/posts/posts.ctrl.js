462c2ddebc69864d149a66b74819064e936dec80
Marco <cwhong8087@gmail.com>
2023-09-26 15:05:11 +0900

    updated

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,5 +1,6 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
+import Joi from '../../../node_modules/joi/lib/index';
 
 const { ObjectId } = mongoose.Types;
 
@@ -21,6 +22,20 @@ export const checkObjectId = (ctx, next) => {
     }
 */
 export const write = async (ctx) => {
+    const schema = Joi.object().keys({
+        title: Joi.string().required(),
+        body: Joi.string().required(),
+        tags: Joi.array().items(Joi.string()).required(),
+    });
+
+    // 검증 실패 시 에러처리
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400; // Bad Request
+        ctx.body = result.error;
+        return;
+    }
+
     const { title, body, tags } = ctx.request.body;
     const post = new Post({
         title,
@@ -87,6 +102,18 @@ export const remove = async (ctx) => {
 */
 export const update = async (ctx) => {
     const { id } = ctx.params;
+    const schema = Joi.object().keys({
+        title: Joi.string(),
+        body: Joi.string(),
+        tags: Joi.array().items(Joi.string()),
+    });
+    // 검증 실패 시 에러처리
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400; // Bad Request
+        ctx.body = result.error;
+        return;
+    }
     try {
         const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
             new: true,
