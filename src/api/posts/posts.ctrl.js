5336baf3be0859677f1d4363ef8b1d041a8e2ada
Marco <cwhong8087@gmail.com>
2023-09-26 14:29:53 +0900

    updated

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,7 +1,55 @@
 import Post from '../../models/post';
 
-export const write = (ctx) => {};
-export const list = (ctx) => {};
-export const read = (ctx) => {};
+/*  데이터 생성(작성)
+    POST /api/posts
+    {
+        title: 제목,
+        body: 내용,
+        tags: [태그1, 태그2]
+    }
+*/
+export const write = async (ctx) => {
+    const { title, body, tags } = ctx.request.body;
+    const post = new Post({
+        title,
+        body,
+        tags,
+    });
+    try {
+        await post.save();
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+
+/*  데이터 조회
+    GET /api/posts
+*/
+export const list = async (ctx) => {
+    try {
+        const posts = await Post.find().exec();
+        ctx.body = posts;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+
+/*  특정 포스트 조회
+    GET /api/posts/:id
+*/
+export const read = async (ctx) => {
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findById(id).exec();
+        if (!post) {
+            ctx.status = 404; // Not Found
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
 export const remove = (ctx) => {};
 export const update = (ctx) => {};
